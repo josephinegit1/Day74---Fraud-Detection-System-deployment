fc41a6dca5b78f839862ec529f6bef0639cb4aeb
josephinegit1 <josephinemary46@gmail.com>
2025-10-18 15:56:18 +0530

    removed gunicorn

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ model = joblib.load("fraud_detection_model.pkl")
 class Transaction(BaseModel):
     features: list  # [value1, value2, ..., valueN]
 
+@app.get("/")
+def read_root():
+    return {"message": "Fraud Detection API is running"}
+
+
 @app.post("/predict")
 def predict(transaction: Transaction):
     data = np.array(transaction.features).reshape(1, -1)
